9c5747556fd2fef19ccae22df2797d2e0a215839
ahmed7370 <ahmedaltafahmedaltaf2@gmail.com>
2024-03-24 18:13:00 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("btn-1").onclick=function(){
+
+    document.getElementById("para").innerHTML= "if string=(ahmad) and num=(123) so then string conatenate with the num "
+
+    document.getElementById("parra").innerHTML= "ahmad 123"
+   
+}
+document.getElementById("btn-2").onclick=function(){
+
+    let Name=prompt("Enter Your Name")
+    console.log("Name=", Name)
+}
+
+document.getElementById("btn-3").onclick=function(){
+
+        document.getElementById("para").innerHTML= "2!==2"
+
+        document.getElementById("parra").innerHTML= "False"
+   
+}
+
+document.getElementById("btn-4").onclick=function(){
+
+        document.getElementById("para").innerHTML= "if(mark>=90)"
+
+        document.getElementById("parra").innerHTML= "You Got A+ Grade"
+   
+}
